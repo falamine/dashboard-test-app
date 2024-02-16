928388717f578dc7e4ac4d2ea422e95cb43b2ed0
frehiwot <frehiwot8@gmail.com>
2024-02-16 18:48:35 +0300

    add tailwind css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/css/bootstrap.css'
+import '../src/app/globals.css'
 import type { AppProps } from 'next/app'
 import Home from "./index";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 
 function MyApp({ Component, pageProps}: AppProps) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -70,58 +70,96 @@ const Home: NextPage = () => {
     }, [dataParams]);
 
     return(
-        <div className='container-md m-5 p-5'>
-            <div className='row mb-3'>
-                <form className='row' onSubmit={search}>
-                    <div className='col-4'>
-                        <select className='form-select' onChange={event => setSearchBy(event.target.value)} required defaultValue={' '}>
-                            <option value='' disabled>Search by</option>
-                            <option value='id'>Transaction ID</option>
-                            <option value='amount'>Amount</option>
-                            <option value='currency'>Currency</option>
-                            <option value='sender'>Sender</option>
-                            <option value='receiver'>Receiver</option>
-                        </select>
-                    </div>
-                    <div className='col-6'>
-                        <input className='form-control' type='text' placeholder='search field'
-                               onChange={event => setSearchParam(event.target.value)} required/>
-                    </div>
-                    <div className='col-2'>
-                        <button type='submit' className='btn btn-primary'>Search</button>
-                    </div>
-                </form>
-            </div>
+        <div className="flex flex-col gap-2">
+
+                    <form className='w-full' onSubmit={search}>
+                        <div className='flex flex-wrap gap-2 align-middle'>
+                            <div>
+                                <select
+                                    className='block appearance-none w-full  border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+                                    onChange={event => setSearchBy(event.target.value)} required
+                                    defaultValue={' '}>
+                                    <option value='' disabled>Search by</option>
+                                    <option value='id'>Transaction ID</option>
+                                    <option value='amount'>Amount</option>
+                                    <option value='currency'>Currency</option>
+                                    <option value='sender'>Sender</option>
+                                    <option value='receiver'>Receiver</option>
+                                </select>
+                            </div>
+                            <div className='grow'>
+                                <input
+                                    className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="90210"'
+                                    type='text' placeholder='search field'
+                                    onChange={event => setSearchParam(event.target.value)} required/>
+                            </div>
+                            <div className=''>
+                                <button type='submit' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Search</button>
+                            </div>
+
+                        </div>
+
+                    </form>
+
 
             {!error &&
                 <>
-                    <div className='list-group mb-2'>{transactions?.data?.map(tr =>
-                        <div className='list-group-item' key={tr.id}>
-                            <div className="d-flex w-100 justify-content-between">
-                                <h6 className="mb-1">{tr.id}</h6>
-                                <small>{tr.created_at_time}</small>
-                            </div>
-                            <div className="d-flex w-100 justify-content-between">
-                                <div>
-                                    <p>{tr.sender.name}</p>
-                                    <p>{tr.receiver.name}</p>
-                                </div>
-                                <small>{tr.amount_with_currency}</small>
-                            </div>
-                            <small>{tr.cause}</small>
-                        </div>
-                    )}</div>
+                    <div className="">{transactions?.data?.map(tr =>
+                            <div className='w-100 bg-white border-bottom border-black my-1' key={tr.id}>
+                                <div className="flex p-2">
+                                    <div className="grow">
+                                        <p className="font-bold text-lg">
+                                            {tr.sender.name} <i className="bi bi-arrow-right"></i> {tr.receiver.name}
+                                        </p>
+                                        <div>
+                                            <code className="p-1 rounded bg-red-100">
+                                                {tr.id.substring(0, 8)}
+                                                &nbsp;
+                                                <button title="Copy Code"><i className="bi bi-copy"></i></button>
+                                            </code>
+                                        </div>
+                                        {tr.cause
+                                            ? <div>
+                                                <i className="bi bi-info-circle"></i> {tr.cause}
+                                            </div>
+                                        : <></>}
 
-                    <Pagination dataParams={dataParams} setDataParams={setDataParams} pageCount={pageCount}/>
-                </>
-}
-            {error && <>
-                <button className=' btn btn-secondary' onClick={router.reload}>Back</button>
-                <p color='red'>No Data Found</p></>}
+                                    </div>
+                                    <div className="text-right">
+                                        <div className="font-bold text-2xl text-gray-500">
+                                            {tr.amount_with_currency}
+                                        </div>
+                                        <div>
+                                            <span>
+                                                <i className="bi bi-calendar-date text-gray-300"></i> {new Date(1000*tr.created_at_time).toLocaleDateString()}
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                                {/*<div className="d-flex w-100 justify-content-between">
+                                    <h6 className="mb-1">{tr.id}</h6>
+
+                                </div>
+                                <div className="d-flex w-100 justify-content-between">
+                                    <div>
+
+                                    </div>
+
+                                </div>
+                                <small>{tr.cause}</small>*/}
+                            </div>
+                        )}</div>
+
+                        <Pagination dataParams={dataParams} setDataParams={setDataParams} pageCount={pageCount}/>
+                    </>
+                }
+                {error && <>
+                    <button className=' btn btn-secondary' onClick={router.reload}>Back</button>
+                    <p color='red'>No Data Found</p></>}
         </div>
 
 
-    )
+)
 }
 
 
